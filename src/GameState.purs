17c581b18a4cafcf75b4e03e1b5a6b084b1c3683
asweingarten <asweingarten+github@gmail.com>
2016-05-24 00:04:27 -0600

    Used a monad and understood it
    - Parse user input into a Space and column number

--- src/GameState.purs
+++ src/GameState.purs
@@ -6,16 +6,48 @@ import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.State.Trans
 import Control.Monad.Eff.Class
 
+import Data.Tuple
+import Data.Maybe
+import Data.List
+import Data.List.WordsLines
+import Data.Int
+
 import Types
 import ReadLine
 
 gameState :: forall e. Game e String
 gameState = do
+  loop
   move <- liftEff $ readLine
   liftEff $ log move
-  (z :: Board) <- get
-  modify (addPiece Red 1)
-  return "Dawgs"
+  --(z :: Board) <- get
+  --modify (addPiece Red 1)
+  return "Congrats someone won"
+ where
+   loop = do
+     move <- liftEff readLine
+     liftEff $ log move
+     let x = parseMove move
+     -- parse move
+     -- modify (addPiece move.Color move.col)
+     -- if !winner
+     loop
+
+parseMove :: String -> Maybe (Tuple Space Int)
+parseMove move = do
+  let moveWords = words move
+  space <- (head moveWords) >>= parseSpace
+  position <- (last moveWords) >>= parsePosition
+  return $ Tuple space position
+
+parseSpace :: String -> Maybe Space
+parseSpace s = case s of
+                 "Red" -> Just Red
+                 "Black" -> Just Black
+                 _ -> Nothing
+
+parsePosition :: String -> Maybe Int
+parsePosition s = fromString s
 
 addPiece :: Space -> Int -> Board -> Board
 addPiece move col board = board
